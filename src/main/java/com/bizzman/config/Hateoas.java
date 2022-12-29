3ec0fe9b9be1f5cb6521ed063a74eaaab3c718f9
Aziz Serin <azizserin313@yahoo.com>
2022-12-29 15:48:04 +0300

    Do some refactoring before implementing jwt based rest authentication service

--- src/main/java/com/bizzman/config/Hateoas.java
+++ src/main/java/com/bizzman/config/Hateoas.java
@@ -15,7 +15,7 @@ public class Hateoas implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        config.setBasePath("/rest");
+        config.setBasePath("/api");
         log.info("Set REST API base path to '/rest'");
 
         config.setLinkRelationProvider(new EvoInflectorLinkRelationProvider());
