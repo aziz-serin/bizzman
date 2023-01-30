a307f0e7a04d4b2b614e184999100d27b5776e0c
Aziz Serin <azizserin313@yahoo.com>
2023-01-30 17:29:51 +0000

    Add test helper to obtain tokens

---
+++ src/test/java/com/bizzman/utils/TestTokenObtain.java
@@ -0,0 +1,37 @@
+package com.bizzman.utils;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class TestTokenObtain {
+    private static String token;
+
+    public static String obtainTokenForAdmin(WebTestClient client) {
+        client.post().uri("/rest/authentication/authenticate")
+                .header("username", "admin")
+                .header("password", "administrator")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBody(Map.class)
+                .consumeWith(result -> {
+                    token = (String) Objects.requireNonNull(result.getResponseBody()).get("token");
+                });
+        return token;
+    }
+
+    public static String obtainTokenForEmployee(WebTestClient client) {
+        client.post().uri("/rest/authentication/authenticate")
+                .header("username", "employee1")
+                .header("password", "employee")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBody(Map.class)
+                .consumeWith(result -> {
+                    token = (String) Objects.requireNonNull(result.getResponseBody()).get("token");
+                });
+        return token;
+    }
+}
