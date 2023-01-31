043f442a9a4b2bb0ce5d8c7d904b79df9e1884a7
Aziz Serin <azizserin313@yahoo.com>
2023-01-31 13:21:14 +0000

    Update test config to use randomised port

--- src/test/java/com/bizzman/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/bizzman/controllers/AuthenticationControllerTest.java
@@ -1,23 +1,23 @@
 package com.bizzman.controllers;
 
 import com.bizzman.BizzmanApplication;
+import com.bizzman.utils.TestTokenObtain;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = BizzmanApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext
 @ActiveProfiles("test")
 public class AuthenticationControllerTest {
-    @Value("${server.port}")
+    @LocalServerPort
     private int port;
 
     private WebTestClient client;
@@ -26,5 +26,4 @@ public class AuthenticationControllerTest {
     public void setup() {
         client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
     }
-
 }
